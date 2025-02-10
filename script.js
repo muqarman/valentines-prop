888fe086dbe3152bc8ff1fcd6f17beca21618efc
muqarman <ricksmalonii@gmail.com>
2025-02-10 20:07:50 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("heart-button").addEventListener("click", function() {
+    document.getElementById("message").style.display = "block";
+});
+
+document.getElementById("yes-button").addEventListener("click", function() {
+    alert("I am so happy you said yes! 💖");
+});
