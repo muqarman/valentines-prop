4846415a397792a16d81e0204c28547e5a341dbd
muqarman <ricksmalonii@gmail.com>
2025-02-10 23:15:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('yesButton').addEventListener('click', function() {
-    window.location.href = 'page2.html'; // Navigate to the second page
+    window.location.href = 'index2.html'; // Navigate to the second page
 });
